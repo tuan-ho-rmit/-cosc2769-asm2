347d8d57b457650e9e3a5f93ec78a043cf6711ab
ChoJaesuk <s3914532@rmit.edu.vn>
2024-08-13 19:26:35 +0700

    Post features
    developed the frontend for the home news feed, and implemented Create, Edit(basic), Delete Features.

---
+++ frontend/src/components/post/ListOfPosts.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import DropDowns from "./DropDowns";
+export default function ListOfPosts({posts, onPostEdit, onPostDelete}) {
+    
+    const post = posts.map((each, index) => {
+        return (
+            <div key={index} className="postContainer">
+                <div className="postHeader">
+                    <div className="imgContainer">
+                    <div className='mx-4'>
+              <img src='https://scontent.fhan4-3.fna.fbcdn.net/v/t39.30808-6/454539036_476839358283813_2967004303045072180_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=-9m-zIRhoLYQ7kNvgF3oBm_&_nc_ht=scontent.fhan4-3.fna&oh=00_AYAcyZp-BRKgTQ_HzRZA-G0OMKGRy4C84B3jhXkfw6A58g&oe=66C0DFF9'
+                   className='w-10 h-10 ring-yellow ring-2 rounded-full' alt='rounded-avatar'/>
+            </div>
+                    </div>
+                    <div className="postInfo">
+                        <div className="userName">
+                            <p>name</p>
+                        </div>
+                        <div className="postDate">
+                            <p>date dd/mm/yyyy</p>
+                        </div>
+                    </div>
+                    <div className="dropDown">
+                        <DropDowns onEdit={() => onPostEdit(index)} onDelete={() => onPostDelete(index)} />
+                    </div>
+                </div>
+                <div className="postContent">
+                    {each}
+                </div>
+                <hr className="solid"></hr>
+                <div className="likeComment">
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        )
+    })
+    return (
+        <div className="postListContainer">
+            {post}
+        </div>
+    )
+}   
--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,7 +1,44 @@
 import React from 'react'
+import CreatePost from '../../components/post/CreatePost';
+import ListOfPosts from '../../components/post/ListOfPosts';
+import { useState } from 'react';
+import './Home.css';
 
 export default function Home() {
+
+  const [posts, setPostList] = useState([]);
+  const [text, setText] = useState("");
+  // 이미지는 나중에
+
+  function handleAddPost() {
+      setPostList([...posts, text])
+      setText("")
+  }
+
+  function handleInput(newPost) {
+      setText(newPost)
+  }
+
+  function handleEditPost(index) {
+    const newText = prompt("Edit your post:", posts[index]);
+    if (newText !== null) {
+        const updatedPosts = [...posts];
+        updatedPosts[index] = newText;
+        setPostList(updatedPosts);
+    }
+}
+
+function handleDeletePost(index) {
+    if (window.confirm("Are you sure you want to delete this post?")) {
+        const updatedPosts = posts.filter((_, i) => i !== index);
+        setPostList(updatedPosts);
+    }
+}
+
   return (
-    <div>Home</div>
+      <div className="mainContent">
+          <CreatePost text={text} onAdd={handleAddPost} onPostChange={handleInput}/>
+          <ListOfPosts posts={posts} onPostEdit={handleEditPost} onPostDelete={handleDeletePost}/>
+      </div>
   )
 }
