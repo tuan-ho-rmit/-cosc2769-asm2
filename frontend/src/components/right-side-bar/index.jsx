f0f20ab6d802af6e74ad5b670391dcc98db5bf93
Doanh Tran <s3927023@rmit.edu.vn>
2024-08-21 18:05:39 +0700

    - finished displaying pending groups

--- frontend/src/components/right-side-bar/index.jsx
+++ frontend/src/components/right-side-bar/index.jsx
@@ -1,22 +1,35 @@
 import React, {useContext} from 'react'
 import { NotificationsContext } from './NotificationsContext.jsx'
+import {NavLink} from "react-router-dom";
 // import useNotifications from "./useNotifications.jsx";
 
 export default function RightSideBar() {
     // const {notifications, unreadNotifications, updateNotificationsUrl} = useNotifications();
     const {notifications, unreadNotifications, pendingGroups} = useContext(NotificationsContext);
-
+    console.log('pending groups from rightsidebar: ', pendingGroups);
 
     return (
         <>
             <div className='conent-end bg-black h-[100vh] w-[250px] right-0 absolute border-white border-l-1'>
-            {/*<div className='bg-black h-[100vh] w-[250px] border-white border-l-1'>*/}
                 <nav>
                     <ul>
+                        <div className='p-2 border-b border-white bg-black text-yellow'>
+                            Group Notifications: {unreadNotifications}
+                        </div>
                         {pendingGroups.length > 0 ? (
                             pendingGroups.map((group) => (
-                                <li key={group.id}
+                                <li key={group._id}
                                     className='p-2 border-b border-white bg-black text-white'>
+                                    <NavLink to='/groups'>
+                                        <div>
+                                            Group: {group.groupName}
+                                        </div>
+                                        <div>
+                                            Created at: {new Date(group.createdAt).toLocaleString(undefined, { month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' })}
+                                            <br/>
+                                            Status: {group.status}
+                                        </div>
+                                    </NavLink>
                                 </li>
                             ))
                         ) : (
