e72f5def75f5baf5fab5bab3408a4864d3b59e42
Jimin76 <jimincho0276@gmail.com>
2024-09-08 00:23:58 +0700

    made manage group post

--- frontend/src/pages/routes.jsx
+++ frontend/src/pages/routes.jsx
@@ -10,6 +10,7 @@ import UserDetails from "./user/UserDetails";
 import ConfigPage from "./config-page";
 import CreateGroup from "./group/creategroup";
 import ManageGroup from "./managegroup";
+import ManageGroupPosts from "./managegroupposts";
 import Admin from "./admin";
 import AdminUsers from "./admin/components/users";
 import AdminGroups from "./admin/components/groups";
@@ -34,6 +35,7 @@ export const router = createBrowserRouter([
       { path: 'groups', element: <ProtectedRoute element={<Group />} /> },
       { path: 'creategroup', element: <ProtectedRoute element={<CreateGroup />} /> },
       { path: 'managegroup', element: <ProtectedRoute element={<ManageGroup />} /> },
+      { path: 'manage-group-posts/:groupName', element: <ProtectedRoute element={<ManageGroupPosts />} /> },
       { path: 'discovergroup', element: <ProtectedRoute element={<DiscoverGroup />} /> },
       { path: 'groupmembermanagement/:groupName', element: <ProtectedRoute element={<GroupMemberManagement />} /> },
       { path: 'groupmain/:groupId', element: <ProtectedRoute element={<GroupMain />} /> },
