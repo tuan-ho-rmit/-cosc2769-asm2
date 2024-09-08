7bb7b83804f6dd84f7df814348b2f8b77ff60167
ChoJaesuk <s3914532@rmit.edu.vn>
2024-09-08 12:35:22 +0700

    deleted edit my profile
    deleted edit my profile

--- frontend/src/pages/user/MyDetails.jsx
+++ frontend/src/pages/user/MyDetails.jsx
@@ -119,40 +119,10 @@ export default function MyDetails() {
                 <div className="userProfileContainer">
                     <span className="profileImgContainer">
                         <img src={avatar} alt="userImage" className="profileImg" />
-                        {isEditing && (
-                            <input
-                                type="file"
-                                accept="image/*"
-                                onChange={handleAvatarChange}
-                                className="changeProfileImg"
-                            />
-                        )}
                     </span>
                     <span className="userName">
-                        {isEditing ? (
-                            <input
-                                type="text"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                className="nameInput"
-                            />
-                        ) : (
                             <p>{name}</p>
-                        )}
-                        <button onClick={() => setIsEditing(!isEditing)} className="changeNameBtn">
-                            <svg className="h-8 w-8 text-yellow-500" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
-                                <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
-                                <line x1="16" y1="5" x2="19" y2="8" />
-                            </svg>
-                        </button>
                     </span>
-                    {isEditing && (
-                        <span className="editBtn">
-                            <button onClick={handleSaveChanges}>Save New Changes</button>
-                        </span>
-                    )}
                 </div>
             </div>
             <hr className="solid"></hr>
